f7fe9c9a1725afa36ae760a392f3db5b9e9048df
ImanolMuracioli <imamuracioli@gmail.com>
2021-11-19 00:01:27 -0300

    Cases Controller params

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,2 +1,16 @@
 class CasesController < ApplicationController
+    def index
+        @cases = json_params
+
+        render json: @cases
+    end
+
+    def json_params
+        #params.permit(:case_id,:assignee,:team,:timestamp)
+        #params.permit(state: [:from, :to], :case_id ) 
+
+        params.require(:_json).map do |param| 
+            param.permit(:case_id, :assignee, :team,{state: [:from, :to]}, :timestamp).to_h
+        end        
+    end
 end
